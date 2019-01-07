9aabf92d31dd3df37c5b3ff90c873b86be543f99
dk <dracula337435@163.com>
2019-01-07 17:11:19 +0800

    一个可用的泛化实现

--- src/main/java/org/dracula/test/dubbo/generic/provider/TestInterfaceImpl.java
+++ src/main/java/org/dracula/test/dubbo/generic/provider/TestInterfaceImpl.java
@@ -1,16 +1,29 @@
 package org.dracula.test.dubbo.generic.provider;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.service.GenericException;
+import com.alibaba.dubbo.rpc.service.GenericService;
 import org.dracula.test.dubbo.generic.TestInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author dk
  */
-@Service
-public class TestInterfaceImpl implements TestInterface {
+@Service(interfaceClass = TestInterface.class)
+public class TestInterfaceImpl implements GenericService {
+
+    private static Logger logger = LoggerFactory.getLogger(TestInterfaceImpl.class);
 
     @Override
-    public String sayHell(String name) {
-        return "hello "+name;
+    public Object $invoke(String method, String[] parameterTypes, Object[] args) throws GenericException {
+        logger.info("收到请求，方法：" + method);
+        logger.info("参数类型：" + Arrays.stream(parameterTypes).collect(Collectors.joining(",")));
+        logger.info("参数：" + Arrays.stream(args).map(Object::toString).collect(Collectors.joining(",")));
+        return null;
     }
+
 }
